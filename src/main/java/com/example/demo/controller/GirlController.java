6602845b97b721cc8e408071e4834eafd9a69d92
zjzj <2454886078@qq.com>
2018-10-14 17:11:05 +0800

    1

--- src/main/java/com/example/demo/controller/GirlController.java
+++ src/main/java/com/example/demo/controller/GirlController.java
@@ -5,82 +5,84 @@ import com.example.demo.domain.Girl;
 import com.example.demo.repository.GirlRepository;
 import com.example.demo.service.GirlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class GirlController {
 
-    @Autowired
-    private GirlRepository girlRepository;
+  @Autowired
+  private GirlRepository girlRepository;
 
-    @Autowired
-    private GirlService girlService;
+  @Autowired
+  private GirlService girlService;
 
-    /**
-     * 查询女生列表
-     *
-     * @return
-     */
-    @GetMapping(value = "/girls")
-    public List<Girl> girlList() {
-        return girlRepository.findAll();
+  /**
+   * 查询女生列表
+   *
+   * @return
+   */
+  @GetMapping(value = "/girls")
+  public List<Girl> girlList() {
+    return girlRepository.findAll();
+  }
+
+  /**
+   * 添加一个女生
+   * @return
+   */
+
+  @PostMapping(value = "/girls")
+  public Girl girlAdd(@Valid  Girl girl, BindingResult bindingResult) {
+    if(bindingResult.hasErrors()){
+      System.out.println(bindingResult.getFieldError().getDefaultMessage());
+      return null;
     }
+    girl.setCupSize(girl.getCupSize());
+    girl.setAge(girl.getAge());
 
-    /**
-     * 添加一个女生
-     *
-     * @param cupSize
-     * @param age
-     * @return
-     */
+    return girlRepository.save(girl);
+  }
 
-    @PostMapping(value = "/girls")
-    public Girl girlAdd(@RequestParam("cupSize") String cupSize,
-                        @RequestParam("age") Integer age) {
-        Girl girl = new Girl();
-        girl.setCupSize(cupSize);
-        girl.setAge(age);
+  //查询一个女生
+  @GetMapping(value = "/girls/{id}")
+  public Girl girlFindOne(@PathVariable("id") Integer id) {
+    return girlRepository.findById(id).orElse(null);
+  }
 
-        return girlRepository.save(girl);
-    }
+  //更新
+  @PutMapping(value = "/girls/{id}")
+  public Girl girlUpdate(@PathVariable("id") Integer id,
+                         @RequestParam("cupSize") String cupSize,
+                         @RequestParam("age") Integer age) {
+    Girl girl = new Girl();
+    girl.setId(id);
+    girl.setCupSize(cupSize);
+    girl.setAge(age);
 
-    //查询一个女生
-    @GetMapping(value = "/girls/{id}")
-    public Girl girlFindOne(@PathVariable("id") Integer id) {
-        return girlRepository.findById(id).orElse(null);
-    }
+    return girlRepository.save(girl);
 
-    //更新
-    @PutMapping(value = "/girls/{id}")
-    public Girl girlUpdate(@PathVariable("id") Integer id,
-                           @RequestParam("cupSize") String cupSize,
-                           @RequestParam("age") Integer age) {
-        Girl girl = new Girl();
-        girl.setId(id);
-        girl.setCupSize(cupSize);
-        girl.setAge(age);
+  }
 
-        return girlRepository.save(girl);
+  //删除
+  @DeleteMapping(value = "/girls/{id}")
+  public void girlDelete(@PathVariable("id") Integer id) {
+    girlRepository.deleteById(id);
+  }
 
-    }
+  //年龄查询
+  @GetMapping(value = "/girls/age/{age}")
+  public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
+    return girlRepository.findByAge(age);
+  }
 
-    //删除
-    @DeleteMapping(value = "/girls/{id}")
-    public void girlDelete(@PathVariable("id") Integer id) {
-        girlRepository.deleteById(id);
-    }
-
-    //年龄查询
-    @GetMapping(value = "/girls/age/{age}")
-    public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
-        return girlRepository.findByAge(age);
-    }
-
-    @PostMapping(value = "/girls/two")
-    public void girlTwo(){
-          girlService.insertTwo();
-    }
+  @PostMapping(value = "/girls/two")
+  public void girlTwo() {
+    girlService.insertTwo();
+  }
 
 }
--- src/main/java/com/example/demo/domain/Girl.java
+++ src/main/java/com/example/demo/domain/Girl.java
@@ -3,6 +3,7 @@ package com.example.demo.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 
 @Entity
 public class Girl {
@@ -12,6 +13,7 @@ public class Girl {
 
     private String  cupSize;
 
+    @Min(value = 18,message = "error")
     private Integer age;
 
     public Girl(){
